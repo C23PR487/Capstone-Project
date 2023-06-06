2975cdf4a5bb06e452fb376042df2d2899d0b45c
Samuel Panggabean <sampanggabean22@gmail.com>
2023-06-06 14:05:57 +0700

    Change models

--- MD/app/src/main/java/io/github/c23pr487/lapakin/model/Lapak.kt
+++ MD/app/src/main/java/io/github/c23pr487/lapakin/model/Lapak.kt
@@ -1,10 +1,20 @@
 package io.github.c23pr487.lapakin.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Lapak(
-    val pictureUrls: List<String>,
+    val pictureUrl: String,
+    val gmapsUrl: String,
+    val name: String,
     val description: String,
     val price: Int,
-    val details: LapakDetails,
-    val seller: Seller,
-    val locationUrl: String,
-)
+    val buildingArea: Int,
+    val city: String,
+    val subDistrict: String,
+    val address: String,
+    val sellerName: String?,
+    val contact: String,
+    val label: String,
+    ) : Parcelable
