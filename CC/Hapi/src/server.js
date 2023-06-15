6a042b2810a884c25b48b2ab78e5daa83b536c30
priyanaadimerta <priyanaa678@gmail.com>
2023-06-15 13:30:30 +0800

    Server Update

--- CC/Hapi/src/server.js
+++ CC/Hapi/src/server.js
@@ -1,16 +1,23 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const db = require('./db_config');
 
 const init = async () => {
   const server = Hapi.server({
     port: 8080,
-    host: '0.0.0.0',
+    host: 'localhost',
   });
 
   server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
+
+  /* Connect to database */
+  db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
 };
 
 init();
