8a49b825f74e9f85e1a8a55ae18ec3b4aabe9fb4
priyanaadimerta <priyanaa678@gmail.com>
2023-06-16 05:12:28 +0800

    Update Server

--- CC/Hapi/src/handler.js
+++ CC/Hapi/src/handler.js
@@ -1,47 +1,35 @@
+/* eslint-disable no-undef */
 const { nanoid } = require('nanoid');
 const db = require('./db_config');
 
 const getTestServerHandler = () => new Promise((resolve) => {
-    resolve({ message: 'Bisa nih aman!' });
+  resolve({ message: 'Bisa nih aman!' });
 });
 
 const dbTable = 'fake_dataset';
 // Retrieve all data in database
 const getAllDataHandler = () => (
-    new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM ${dbTable}`;
-        db.query(sql, (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
-        });
-    })
+  new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM ${dbTable}`;
+    db.query(sql, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  })
 );
 
 const getFilteredDataHandler = (request) => {
-    const {
-        label = null, prior1 = null, prior2 = null, prior3 = null,
-        value1 = null, value2 = null, value3 = null,
-    } = request.query;
-    return new Promise((resolve, reject) => {
-        // if (label === NULL){
-        //     const sql = `
-        //         SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
-        //         FROM ${dbTable}
-        //         WHERE ${prior1} = ${value1} AND ${prior2} = ${value2} AND ${prior3} = ${value3} 
-        //         ORDER by harga`;
-        //     db.query(sql, (err, res) => {
-        //         if (err) {
-        //             reject(err);
-        //         }
-        //         resolve(res);
-        //     });
-        // } else {
-            if (prior1 === 'kecamatan') {
-                if (value1 === NULL) {
-                    const sql = `
+  const {
+    label = null, prior1 = null, prior2 = null, prior3 = null,
+    value1 = null, value2 = null, value3 = null,
+  } = request.query;
+  return new Promise((resolve, reject) => {
+    if (prior1 === 'kecamatan') {
+      if (value1 === NULL) {
+        const sql = `
                         WITH second_important AS (
                             SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
                             FROM ${dbTable}
@@ -58,14 +46,14 @@ const getFilteredDataHandler = (request) => {
                         UNION ALL
                         SELECT * FROM second_important
                         ) as combined_result;`;
-                    db.query(sql, (err, res) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve(res);
-                    });
-                } else {
-                    const sql = `
+        db.query(sql, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      } else {
+        const sql = `
                         WITH third_important AS (
                             SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
                             FROM ${dbTable}
@@ -89,16 +77,16 @@ const getFilteredDataHandler = (request) => {
                         UNION ALL
                         SELECT * FROM third_important
                         ) as combined_result;`;
-                    db.query(sql, (err, res) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve(res);
-                    });
-                }
-            }
-            if (prior1 === 'kota') {
-                const sql = `
+        db.query(sql, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      }
+    }
+    if (prior1 === 'kota') {
+      const sql = `
                         WITH second_important AS (
                             SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
                             FROM ${dbTable}
@@ -115,17 +103,17 @@ const getFilteredDataHandler = (request) => {
                         UNION ALL
                         SELECT * FROM second_important
                         ) as combined_result;`;
-                db.query(sql, (err, res) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(res);
-                });
-            };
-            if (prior1 === 'harga') {
-                const valueHarga = parseInt(value1, 10);
-                if (prior2 === 'kecamatan' && prior2 === NULL) {
-                    const sql = `
+      db.query(sql, (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      });
+    }
+    if (prior1 === 'harga') {
+      const valueHarga = parseInt(value1, 10);
+      if (prior2 === 'kecamatan' && prior2 === NULL) {
+        const sql = `
                         WITH third_important as (
                             SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
                             FROM ${dbTable}
@@ -149,15 +137,14 @@ const getFilteredDataHandler = (request) => {
                         UNION ALL
                         SELECT * FROM third_important
                         ) as combined_result;`;
-                    db.query(sql, (err, res) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve(res);
-                    });
-                }
-                else {
-                    const sql = `
+        db.query(sql, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      } else {
+        const sql = `
                         WITH third_important as (
                             SELECT kota, harga, kecamatan, id, label, alamat, url_thumbnail
                             FROM ${dbTable}
@@ -181,29 +168,29 @@ const getFilteredDataHandler = (request) => {
                         UNION ALL
                         SELECT * FROM third_important
                         ) as combined_result;`;
-                    db.query(sql, (err, res) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve(res);
-                    });
-                };
-            };
-    });
+        db.query(sql, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      }
+    }
+  });
 };
 
 const getDataByIdHandler = (request) => {
-    const { id } = request.params;
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM ${dbTable} WHERE id ='${id}'`;
-        db.query(sql, (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
-        });
+  const { id } = request.params;
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM ${dbTable} WHERE id ='${id}'`;
+    db.query(sql, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
     });
+  });
 };
 
 const addDataHandler = (request) => {
@@ -241,9 +228,9 @@ const addDataHandler = (request) => {
 };
 
 module.exports = {
-    getAllDataHandler,
-    getFilteredDataHandler,
-    getDataByIdHandler,
-    addDataHandler,
-    getTestServerHandler,
+  getAllDataHandler,
+  getFilteredDataHandler,
+  getDataByIdHandler,
+  addDataHandler,
+  getTestServerHandler,
 };
